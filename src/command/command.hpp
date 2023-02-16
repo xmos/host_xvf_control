7ce935a4e51182dba3c2ed1844210042fbbdeda7
Shuchita Khare <shuchitak@xmos.com>
2023-02-16 12:13:55 +0000

    Fixed comment

--- src/command/command.hpp
+++ src/command/command.hpp
@@ -60,7 +60,7 @@ class Command
          * returned from the device.
          * 
          * @param data          Byte array containing the read command
-         * @param payload_len   Length of the byte stream we want to write into the device
+         * @param payload_len   Length of the byte stream to write to the device
          * @note                Only for internal testing.
          */
         control_ret_t command_get_low_level(uint8_t *data, size_t payload_len);
@@ -72,7 +72,7 @@ class Command
          * returns the error code returned from the device.
          *
          * @param cmd           Byte array containing the write command and payload
-         * @param payload_len   Length of the byte stream we want to write into the device
+         * @param payload_len   Length of the byte stream to write to the device
          * @note                Only for internal testing
          */
         control_ret_t command_set_low_level(uint8_t *data, size_t payload_len);
