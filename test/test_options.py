8cb9543c216b9983edc4a11e83caf8491623b47b
uvvpavel <paveluvarov@xmos.com>
2023-02-21 13:13:41 +0000

    adding tests

---
+++ test/test_options.py
@@ -0,0 +1,41 @@
+# Copyright 2023 XMOS LIMITED.
+# This Software is subject to the terms of the XCORE VocalFusion Licence.
+
+import test_utils
+import os
+from pathlib import Path
+
+small_cmd = "CMD_SMALL"
+
+def test_execute_cmd_list():
+    test_dir, host_bin, control_protocol = test_utils.get_dummy_files()
+    print("\n")
+
+    cmd_list_path = test_dir / "commands.txt"
+    if cmd_list_path.is_file(): os.remove(cmd_list_path)
+    with open(cmd_list_path, "w") as f:
+        f.write(small_cmd + " 156   -894564     4586543\n")
+        f.write("\n")
+        f.write(small_cmd)
+
+    # check that it does not fail
+    out = test_utils.execute_command(host_bin, control_protocol, test_dir, "-e")
+    print(out)
+
+def test_version():
+    test_dir, host_bin, control_protocol = test_utils.get_dummy_files()
+    print("\n")
+
+    version = test_utils.execute_command(host_bin, control_protocol, test_dir, "-v")
+    changelog = Path(__file__).parents[1] / "CHANGELOG.rst"
+    assert changelog.is_file(), f"Could not find {changelog}"
+
+    with open(changelog, 'rt') as f:
+        line = f.readline()
+        # will need to change after 1.0.0
+        while line[0] != '0':
+            line = f.readline()
+
+        print(line)
+        assert str(version[0]) == line.strip()
+
