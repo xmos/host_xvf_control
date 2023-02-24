a307faeb4edbd84b5e49393c019f9df56adaa177
shuchitak <shuchitak@xmos.com>
2023-02-24 16:27:43 +0000

    Added documentation to read_aec_filter.py

--- python/read_aec_filter.py
+++ python/read_aec_filter.py
@@ -3,6 +3,25 @@ import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 
+"""
+This script is used for plotting the XVF3800 AEC filter coefficients that are read from the device
+by using the hostapp -gf <filename.bin> option.
+Please note that the input argument to this script is the filename.bin without the .f<x>m<y> extension, so
+the same filename that was used in the -gf option when retrieving the filter from the device.
+
+Requirements:
+    python3
+    numpy
+    matplotlib
+
+Usage
+    python3 read_aec_filter.py <aecfilter.bin>
+
+Normally, this script is run after retrieving the filter coefficients from the device.
+For example, from the <path_to_host_app>/build directory, one would run:
+    sudo ./xvf_host -gf aecfilt.bin
+    python3 ../python/read_aec_filter.py aecfilt.bin
+"""
 def read_aec_filter(filelist, show_plot):
     num_files = len(filelist)
     fig, axs = plt.subplots(num_files, 2, figsize=(14,7))
@@ -15,6 +34,8 @@ def read_aec_filter(filelist, show_plot):
         Buf = np.fft.rfft(buf)
         freq = np.linspace(0, 8000, num=int(len(buf)/2)+1) # Map 0-8kHz into 3072/2 bins
         H = 20*np.log10(np.abs(Buf + 1e-39))
+        print(f"{filelist[i].name}: Magnitude response peak = {np.max(H)} dB")
+
         axs[i,0].plot(buf)
         axs[i,0].set(xlabel='samples', ylabel='Amplitude')
         axs[i,1].plot(freq, H)
@@ -34,13 +55,12 @@ def read_aec_filter(filelist, show_plot):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Read AEC filter file")
+    parser = argparse.ArgumentParser("Plot the AEC filter files that have been read from the device using the host app's -gf option")
     parser.add_argument('aecfilter_file', type=str,
-                        help='Binary file containing the AEC filter buffer read from the device',
+                        help='AEC filter .bin file without the .f<x>m<y> extension',
                         )
     parser.add_argument('--num-mics', '-m', type=int, default=4, help='number of mic inputs. Default=4')
     parser.add_argument('--num-ref', '-r', type=int, default=1, help='number of far end inputs. Default=1')
-    parser.add_argument('--show-plot', '-sp', action='store_true', help='Show the filter plot')
     
     args = parser.parse_args()
     filelist = []
@@ -51,5 +71,6 @@ if __name__ == "__main__":
                 print(f"Error: file {name} not found")
                 exit(1)
             filelist.append(name)
-
-    read_aec_filter(filelist, args.show_plot)
+    
+    show_plot = True
+    read_aec_filter(filelist, show_plot)
