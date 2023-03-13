d8d9cde7efaa169cb30a3e7b4746c37044720684
lucianomartin <lucianom@xmos.com>
2023-03-13 22:04:03 +0000

    Fix test arguments

--- test/test_options.py
+++ test/test_options.py
@@ -49,37 +49,37 @@ def test_range_check():
         # this part of the test will check if the range check works as expected
         vals = test_utils.gen_rand_array('int', 0, 6, 1)
         if ((vals[0] >= 0) and (vals[0] <= 3)) or (vals[0] == 5):
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST0", vals, True)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST0", cmd_vals=vals, expect_success=True)
         else:
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST0", vals, False)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST0", cmd_vals=vals, expect_success=False)
 
         vals = test_utils.gen_rand_array('float', 0.0, 1.1, 3)
         if((vals[0] >= 0.0) and (vals[0] <= 1.0)) and ((vals[1] >= 0.5) and (vals[1] <= 1.0)) and ((vals[2] >= 0.0) and (vals[2] <= 0.5)):
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST1", vals, True)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST1", cmd_vals=vals, expect_success=True)
         else:
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST1", vals, False)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST1", cmd_vals=vals, expect_success=False)
 
         vals = test_utils.gen_rand_array('int', 0, 15, 2)
         if((vals[0] >= 0) and (vals[0] <= 3)) and (((vals[1] >= 0) and (vals[1] <= 6)) or ((vals[1] >= 10) and (vals[1] <= 14))):
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST2", vals, True)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST2", cmd_vals=vals, expect_success=True)
         else:
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST2", vals, False)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST2", cmd_vals=vals, expect_success=False)
 
         vals = test_utils.gen_rand_array('int', 0, 400, 3)
         if((vals[0] >= 0) and (vals[0] <= 255)) and ((vals[2] >= 0) and (vals[2] <= 255)):
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST3", vals, True)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST3", cmd_vals=vals, expect_success=True)
         else:
-            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST3", vals, False)
+            test_utils.execute_command(host_bin, control_protocol, test_dir, "RANGE_TEST3", cmd_vals=vals, expect_success=False)
 
         # here we check that bypass works
         vals = test_utils.gen_rand_array('int', -2147483648, 2147483647, 1)
-        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST0", vals, True)
+        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST0", cmd_vals=vals, expect_success=True)
 
         vals = test_utils.gen_rand_array('float', -2147483648, 2147483647, 3)
-        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST1", vals, True)
+        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST1", cmd_vals=vals, expect_success=True)
 
         vals = test_utils.gen_rand_array('int', 0, 255, 2)
-        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST2", vals, True)
+        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST2", cmd_vals=vals, expect_success=True)
 
         vals = test_utils.gen_rand_array('int', 0, 4294967295, 3)
-        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST3", vals, True)
+        test_utils.execute_command(host_bin, control_protocol, test_dir, "-br RANGE_TEST3", cmd_vals=vals, expect_success=True)
