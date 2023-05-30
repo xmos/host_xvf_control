e8e1bfa7e47bd3026f53d8638e8ef8d7c9e070a7
uvvpavel <paveluvarov@xmos.com>
2023-05-30 11:03:56 +0100

    missing space

--- src/special_commands/special_commands.cpp
+++ src/special_commands/special_commands.cpp
@@ -153,7 +153,7 @@ control_ret_t print_help_menu()
     const size_t hard_stop = get_term_width();
 
     cout << "usage: xvf_host [ command | option ]" << endl
-    << setw(77) << "[ -u <protocol> ] [-cmp <path> ] [ -br ] [ command | option ]" << endl
+    << setw(78) << "[ -u <protocol> ] [ -cmp <path> ] [ -br ] [ command | option ]" << endl
     << endl << "Current application version is " << current_host_app_version << "."
     << endl << "You can use --use or -u option to specify protocol you want to use"
     << endl << "or call the option/command directly using default control protocol."
