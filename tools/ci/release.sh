254b60962051f9e9e4efc9934f40bac0dbc34138
lucianomartin <lucianom@xmos.com>
2023-10-11 12:21:55 +0100

    Add new files

---
+++ tools/ci/release.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+#
+# Script run by CI to create the release package
+
+# Script fails on an unchecked error exit code
+# script echos everything
+# set -ex
+
+title () {
+    #set +x
+    title_len=$(printf "$1" | wc -c)
+    echo
+    echo $1
+    printf "=%.0s" $(seq $title_len)
+    printf "\n"
+    echo
+    #set -x
+}
+
+ROOT_DIR=$(pwd)
+RELEASE_DIR=${ROOT_DIR}/release
+mkdir -p ${RELEASE_DIR}
+title "Files and folders to copy"
+cat $(pwd)/tools/ci/release-sources-allowlist.txt
+title "Create source release package"
+cat $(pwd)/tools/ci/release-sources-allowlist.txt | xargs -I% cp -r --parents %  ${RELEASE_DIR}
+title "Completed"
