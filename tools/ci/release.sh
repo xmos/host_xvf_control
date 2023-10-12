e43b982767e1c18dd596d8e2b4eecfda80c5f191
lucianomartin <lucianom@xmos.com>
2023-10-12 11:38:43 +0100

    Add release notes

--- tools/ci/release.sh
+++ tools/ci/release.sh
@@ -19,12 +19,20 @@ title () {
 
 ROOT_DIR=$(pwd)
 RELEASE_DIR=${ROOT_DIR}/release
+VERSION=$(sed -n '4p' < $(pwd)/CHANGELOG.rst)
+
 mkdir -p ${RELEASE_DIR}
 title "Files and folders to copy"
 cat $(pwd)/tools/ci/release-sources-allowlist.txt
 title "Create source release package"
 cat $(pwd)/tools/ci/release-sources-allowlist.txt | xargs -I% cp -r --parents %  ${RELEASE_DIR}
+RELEASE_NOTES_FILE=${ROOT_DIR}/doc/release_notes/release_notes_v${VERSION}.txt
+if [ -f "${RELEASE_NOTES_FILE}" ]; then
+    cp  ${RELEASE_NOTES_FILE} ${RELEASE_DIR}
+else
+    echo "Warning: release notes for version ${VERSION} not found"
+fi
+
 title "Zip files"
-VERSION=$(sed -n '4p' < $(pwd)/CHANGELOG.rst)
 zip -r host_xvf_control_release_v${VERSION}.zip release
 title "Completed"
